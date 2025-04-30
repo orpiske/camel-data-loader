9bbd13a6c15e9a047300a72dbf62bf8f43b9198d
Otavio Rodolfo Piske <angusyoung@gmail.com>
2025-04-30 11:18:39 +0200

    Removed unused code

--- assistant-ui-vaadin/src/main/java/org/apache/camel/assistant/ui/vaadin/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package org.apache.camel.assistant.ui.vaadin;
-
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
-
-@Theme("starter-theme")
-public class AppConfig implements AppShellConfigurator {
-}
